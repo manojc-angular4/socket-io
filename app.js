1b6886ede7040b5522b9a8a86b2cac0572d09487
Manoj Chalode <chalodem@gmail.com>
2017-09-11 15:11:57 +0100

    added web-coskcet project

--- app.js
+++ app.js
@@ -1,46 +1,27 @@
-var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var app = require('express')();
+var ws = require('express-ws')(app);
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.ws('/feed1', (socket, req) => {
+    socket.on('message', (message) => {
+        ws.getWss().clients.forEach(function(client) {
+            console.log(message + 'feed 1 route server');
+            client.send(message);
+        }, this); 
+    });
 });
 
-module.exports = app;
+app.ws('/feed2', (socket, req) => {
+    socket.on('message', (message) => {
+        ws.getWss().clients.forEach(function(client) {
+            console.log(message + 'feed 2 route server');
+            client.send(message);
+        }, this); 
+    });
+});
+
+app.listen(3000, () => console.log("server running on port 3000"));
